2e3b6406326bf97edfc09427a999c637fa41f349
walala26 <walala26@gmail.com>
2024-05-17 20:31:21 -0500

    Trabajo de clase Vieres 17 de mayo

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
 /* -------------------------- import  -------------------------- */
 import express from "express";
+import morgan from "morgan";
 
+import {join, dirname} from "path"
+import {fileURLToPath} from "url"
+import {engine} from "express-handlebars"
 
 /* ----------------------------- Initialization ----------------------------- */
 const app = express();
+/* ----------------- Evitar Colisiones de nombre de variable ---------------- */
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 
 /* -------------------------------- settings -------------------------------- */
-app.set("port" , process.env.PORT || 3000 )
 
+app.set("port" , process.env.PORT || 3000 );
 
+app.set("views", join(__dirname, "views"));
+app.engine(".hbs", engine({
+    defaultLayout: "main" , 
+    layoutsDir: join(app.get("views"), "layouts"),
+    partialsDir: join(app.get("views"), "partials"),
+    extname: ".hbs"
+}));
+app.set("view engine", ".hbs");
 
 /* ------------------------------- Middlewares ------------------------------ */
 
-
-
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 /* --------------------------------- routes --------------------------------- */
 
-
-
+app.get("/", (req, res) => {
+    res.json({"message": "Hola Mundo"})
+});
 
 /* ------------------------------ Public filess ----------------------------- */
 
-
+app.unsubscribe(express.static(join(__dirname, "public")));
 
 /* ------------------------------- run server ------------------------------- */
-app.listen(app.get("port"), () => {
-    console.log("Server listeing on port" , app.get("port")) ;
 
-} );
+app.listen(app.get("port"), () => {
+    console.log("Server listeing on port" , app.get("port"));
+});
