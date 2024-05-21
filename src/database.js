3e0e44f028bb2d062aa973593bf1f2515aa027e6
walala26 <walala26@gmail.com>
2024-05-20 20:32:32 -0500

    Correccion de archivo database.js

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 import {createPool} from "mysql2/promise"
 
-const pool = createPool((
+const pool = createPool({
     host: "localhost",
     port: "8080",
     user: "prueba01",
     password: "prueba01",
     database: "prueba01"
-));
+});
 
 export default pool;
